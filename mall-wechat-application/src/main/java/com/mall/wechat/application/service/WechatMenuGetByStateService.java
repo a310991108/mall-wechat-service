d59f3a171f152bd5faa52469648a940defd418be
zhangyong <310991108@qq.com>
2025-08-18 19:58:45 +0800

    refactor(mall-wechat-application): 对违法单一职责原则的用例进行拆分，并用空实现去实现用例。
    将WechatMenuUseCase和WechatUserUseCase这两个用例中的功能拆分成若干个相应职责的用例，并创建service来实现它们。
    
    BREAKING CHANGE: 无

--- mall-wechat-application/src/main/java/com/mall/wechat/application/service/WechatMenuGetByStateService.java
+++ mall-wechat-application/src/main/java/com/mall/wechat/application/service/WechatMenuGetByStateService.java
@@ -7,6 +7,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class WechatMenuGetByStateService implements WechatMenuGetByStateUseCase {
+    /**
+     * 根据菜单状态查询菜单列表
+     */
     @Override
     public List<WechatMenuResponse> getWechatMenusByState(Integer menuState) {
         return Collections.emptyList();
